e32ab7b3d29f4f145de20baa135aa23a9fdc521d
Arlen Vasconcelos <arlenvasconcelos@gmail.com>
2021-03-28 17:55:47 -0300

    Create user: add controller and useCase

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,14 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { name, email } = request.body;
+      const newUser = this.createUserUseCase.execute({ name, email });
+
+      return response.status(201).send(newUser);
+    } catch (err) {
+      return response.status(400).send({ error: err.message });
+    }
   }
 }
 
